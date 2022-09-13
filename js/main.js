3a102d36d82023eb681bb11523881dcf376b85fd
Roman <fedoskinrp@gmail.com>
2022-09-13 13:51:29 +1000

    Hw. Added mass and object

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
-let user = 'user name';
-console.log (user);
-user = 5;
-console. log (user)
+// let user = 'user name';
+// console.log (user);
+// user = 5;
+// console. log (user)
+
+const mass = ['one','two','three','four','five']
+
+console.log(mass[2]);
+
+const phone = {
+    color: '#fff',
+    model: 'iphone',
+    price: 50000
+};
+
+
+console.log(phone)
+
+console.log(phone.color);
+console.log(phone.model);
+console.log(phone.price);
+
+
