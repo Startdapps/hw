cad051227060508ea77a71c73d52d30fa9199d9f
Roman <fedoskinrp@gmail.com>
2022-10-04 15:41:38 +1000

    Hw

--- js/main.js
+++ js/main.js
@@ -21,61 +21,72 @@
 // console.log(phone.price);
 
 // 1
-const arr = [1,2,3,4,5,6];
+// const arr = [1,2,3,4,5,6];
 
-for (let i=0; i < arr.length; i++){
-    console.log(arr[i])
-}
+// for (let i=0; i < arr.length; i++){
+//     console.log(arr[i])
+// }
 
 // // 2
 
-const sayHello = () => {
+// const sayHello = () => {
 
-    let firstName = prompt ('Введите имя');
+//     let firstName = prompt ('Введите имя');
     
-    if(firstName == '') {
-        alert('Вы не ввели Имя, введите пожалуйста Имя');
-        sayHello();
-    }; alert (`Имя, ${firstName}`);
-}
+//     if(firstName == '') {
+//         alert('Вы не ввели Имя, введите пожалуйста Имя');
+//         sayHello();
+//     } alert (`Имя, ${firstName}`);
+// }
 
-sayHello()
+// sayHello()
 
-//3
+// //3
 
 
-const getAge = () => {
+// const getAge = () => {
 
-    let age = prompt ('Введите Ваш возраст');
+//     let age = prompt ('Введите Ваш возраст');
 
-    if(age.length <= 3 && age !== 0 && parseInt(age)) {
-        age < 5 ? alert('Вы младенец') :
-        age > 5 && age < 10 ? alert('Вы ребенок') : 
-        age > 10 && age < 18 ? alert('Вы подросток') : 
-        age > 18 && age < 65 ? alert('Вы взрослый') : 
-        age > 65 ? alert('Вы старик') : 
-        age == 5 || age == 10 || age == 18 || age == 65 ? alert(`Извините, у нас в тз только строго > либо < ${age} лет`)
-        : null ;
+//     if(age.length <= 3 && age !== 0 && parseInt(age)) {
+//         age < 5 ? alert('Вы младенец') :
+//         age > 5 && age < 10 ? alert('Вы ребенок') : 
+//         age > 10 && age < 18 ? alert('Вы подросток') : 
+//         age > 18 && age < 65 ? alert('Вы взрослый') : 
+//         age > 65 ? alert('Вы старик') : 
+//         age == 5 || age == 10 || age == 18 || age == 65 ? alert(`Извините, у нас в тз только строго > либо < ${age} лет`)
+//         : null ;
         
-    } else {
-        alert('Пожалуйста вводите только числа, и корректные значения возраста');
-        getAge();
-    }  
+//     } else {
+//         alert('Пожалуйста вводите только числа, и корректные значения возраста');
+//         getAge();
+//     }  
     
-}
+// }
     
-getAge ();
+// getAge ();
 
-// 4
+// // 4
 
-getNumType = () => {
+// getNumType = () => {
 
-    let askNum = prompt('Введите число');
+//     let askNum = prompt('Введите число');
 
-    askNum%3 == 0 && askNum%5 ==0 ? alert('FizzBuzz'): 
-    askNum%3 == 0 ? alert('Fizz'):
-    askNum%5 == 0 ? alert('Buzz'): null;
+//     askNum%3 == 0 && askNum%5 ==0 ? alert('FizzBuzz'): 
+//     askNum%3 == 0 ? alert('Fizz'):
+//     askNum%5 == 0 ? alert('Buzz'): null;
     
-}
+// }
 
-getNumType()
+// getNumType()
+
+// let noDude = 0;
+
+// if ( !noDude) {
+//     console.log('Real deal');
+// } else if (noDude) {
+//     console.log('Hustla')
+// }
+//   Тернарник в тернаринке нельзя
+
+
