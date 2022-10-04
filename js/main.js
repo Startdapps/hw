bec5f71663dfdd4ac6ab292de5c00e9dcb0882c5
Roman <fedoskinrp@gmail.com>
2022-10-04 17:03:24 +1000

    Api hw

--- js/main.js
+++ js/main.js
@@ -90,3 +90,38 @@
 //   Тернарник в тернаринке нельзя
 
 
+const root = document.querySelector('#root')
+
+const makeElement = (tagName, className) => {
+    const element = document.createElement(tagName);
+    element.classList.add(className);
+    return element;
+    
+}
+
+
+
+const getCharacters = async () => {
+    const res = await fetch ('https://rickandmortyapi.com/api/character');
+    const charactersList = await res.json();
+    const { results } = charactersList;
+
+for (let i = 0; i < results.length; i++) {
+
+    
+    const { name , gender, image } = results[i];
+
+    const charName = root.appendChild(makeElement('p','name'));
+    const charGender =  root.appendChild(makeElement('p', 'gender'));
+    const charPicture =  root.appendChild(makeElement ('img','picture'));
+    charName.style.borderTop = 'solid';
+
+    charName.innerText = name;
+    charGender.innerText = gender;
+    charPicture.src = image;
+}
+}
+
+getCharacters()
+
+
